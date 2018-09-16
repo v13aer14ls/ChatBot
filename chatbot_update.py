e2e6e8984a5e670f7c646798a8fe109f317d668e
v13aer14ls <v13aer14ls@gmail.com>
2018-09-15 23:46:29 -0300

    consertando o venv

--- chatbot_update.py
+++ chatbot_update.py
@@ -12,3 +12,17 @@ def last_update(data):
     results = data['result']
     total_updates = len(results) - 1
     return results[total_updates]
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+chat_id = get_chat_id(last_update(get_updates_json(url)))
+
+send_mess(chat_id, 'Your message goes here')
+
